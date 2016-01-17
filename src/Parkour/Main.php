3363f841133887b63c2acca96951a58f7af8e5b7
Driesboy <Driesboy@users.noreply.github.com>
2016-01-17 08:42:25 +0100

    Update Main.php

--- src/Parkour/Main.php
+++ src/Parkour/Main.php
@@ -57,6 +57,7 @@ class Main extends PluginBase implements Listener{
 						$level = $this->getServer()->getLevelByName($pos[3]);
 						if($level) $player->teleport(new Position($pos[0],$pos[1],$pos[2],$level));
 					}else $player->sendMessage("Error");
+				}
           }
      }
 }
