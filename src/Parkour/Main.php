1f9d05185fc6ac41c9041069a4813fbac24144dc
Driesboy <Driesboy@users.noreply.github.com>
2016-01-26 16:33:45 +0100

    Update Main.php

--- src/Parkour/Main.php
+++ src/Parkour/Main.php
@@ -76,7 +76,7 @@ class Main extends PluginBase implements Listener{
 					$player->sendMessage("{$this->getConfig()->get("TeleportMessage")}");
 						$level = $this->getServer()->getLevelByName($pos[3]);
 						$player->teleport(new Position($pos[0],$pos[1],$pos[2],$level));
-					}else $player->sendMessage("No-Checkpoint");
+					}else $player->sendMessage("{$this->getConfig()->get("No-Checkpoint")}");
 					$world = $this->getServer()->getLevelByName($level);
 					$event->getPlayer()->teleport($world->getSafeSpawn());
           }
