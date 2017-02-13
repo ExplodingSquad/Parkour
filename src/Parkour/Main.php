d49199044a54d2548ecaeac5663685702c5bdd45
XShockinFireX <XShockinFireX@users.noreply.github.com>
2017-02-13 05:59:40 +0000

    Features addition. (#37)
    * Reformatted.
    
    * Closes issue #25
    
    Adds features suggested in issue #25.
    
    * Added a config option to remove player data on quit, and fixed an issue.
    
    * Stupid IDE.

--- src/Parkour/Main.php
+++ src/Parkour/Main.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   _____       _           _                 
  |  __ \     (_)         | |                
@@ -9,7 +10,9 @@
                                        __/ |
                                       |___/
 */
+
 namespace Parkour;
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\command\CommandSender;
@@ -19,6 +22,7 @@ use pocketmine\command\CommandExecutor;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 use pocketmine\tile\Sign;
@@ -32,19 +36,20 @@ class Main extends PluginBase implements Listener{
 	
 	private $config;
 	private $pos;
-	public function onEnable(){
+
+	public function onEnable() {
 		$this->getServer()->getLogger()->info(TextFormat::BLUE . "Parkour Has Been Enabled.");
 		$this->getServer()->getLogger()->info(TextFormat::BLUE . "By: NL-4-DEVS http://github.com/NL-4-DEVS");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
-	    	$this->data = new Config($this->getDataFolder()."Data.yml", Config::YAML, array());
+	    $this->data = new Config($this->getDataFolder() . "Data.yml", Config::YAML, array());
 	}
 	
 	public function onDisable(){
 		$this->getServer()->getLogger()->info(TextFormat::GRAY . ">" . TextFormat::RED . "RED" . "Parkour was disabled.");
 	}
 	
-	public function onPlayerTouch(PlayerInteractEvent $event){
+	public function onPlayerTouch(PlayerInteractEvent $event) {
 		$player = $event->getPlayer();
 		$name = strtolower($event->getPlayer()->getName());
 		if($event->getBlock()->getID() === 63 || $event->getBlock()->getID() === 68){ 
@@ -54,18 +59,18 @@ class Main extends PluginBase implements Listener{
 			}
 			$sign = $sign->getText();
 			if(TextFormat::clean($sign->getText()[0]) === '[Checkpoint]'){
-				$this->data->set($name,array($player->x,$player->y,$player->z,$player->getLevel()->getName()));
+				$this->data->set($name, array($player->x, $player->y, $player->z, $player->getLevel()->getName()));
 				$this->data->save();
 				$player->sendMessage($this->getConfig()->get("CheckpointSaved"));
 			}
 			if(TextFormat::clean($sign->getText()[0]) === '[Earn Reward]'){
-				$this->data->remove($name,array($player->x,$player->y,$player->z,$player->getLevel()->getName()));
+				$this->data->remove($name, array($player->x, $player->y, $player->z, $player->getLevel()->getName()));
 				$this->data->save();
 				$player->sendMessage($this->getConfig()->get("EarnReward"));
-				if($this->getConfig()->get("reward-command")){
-					$player->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{PLAYER}", $player->getName(), $this->getConfig()->get("reward-command")));
-					$player->teleport($player->getLevel()->getSafeSpawn());
+				foreach($this->getConfig()->get("reward-commands") as $command){
+					$player->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{PLAYER}", $player->getName(), $command));
 				}
+				$player->teleport($player->getLevel()->getSafeSpawn());
 			}
 		}
 		if($event->getBlock()->getID() === $this->getConfig()->get("CheckPointBlock")){
@@ -74,23 +79,33 @@ class Main extends PluginBase implements Listener{
 			$player->sendMessage($this->getConfig()->get("CheckpointSaved"));
 		}
 	}
-     public function onVoidLoop(PlayerMoveEvent $event){
+
+    public function onVoidLoop(PlayerMoveEvent $event){
      	$player = $event->getPlayer();
      	if($player->getLevel()->getName() === $this->getConfig()->get("world")){
           	if($event->getTo()->getFloorY() < 1){
-             		$player = $event->getPlayer();
-             		$name = $event->getPlayer()->getName();             	
-             		$name = strtolower($name);
-             		$pos = $this->data->get($name);
-			if(is_array($pos)){
+             	$player = $event->getPlayer();
+             	$name = $event->getPlayer()->getName();             	
+             	$name = strtolower($name);
+             	$pos = $this->data->get($name);
+				if(is_array($pos)){
 					$player->sendMessage($this->getConfig()->get("TeleportMessage"));
 					$level = $this->getServer()->getLevelByName($pos[3]);
 					$player->teleport(new Position($pos[0],$pos[1],$pos[2],$level));
-				}else{ 
+				} else { 
 					$player->sendMessage($this->getConfig()->get("No-Checkpoint"));
 					$player->teleport($player->getLevel()->getSafeSpawn());
+				}
 			}
-                }
-          }
-        }
+		}
+	}
+
+	public function onQuit(PlayerQuitEvent $event) {
+		$player = $event->getPlayer();
+		$name = strtolower($player->getName());
+		if($this->getConfig()->get("RemovePlayerDataOnQuit")) {
+			$this->data->remove($name, array($player->x, $player->y, $player->z, $player->getLevel()->getName()));
+			$this->data->save();
+		}
+	}
 }
